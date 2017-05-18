fa26bd7457b9fd6649d29ba71fb82a77503e6c5a
Chetan Kanthala <crkanthala@gmail.com>
2017-05-18 14:18:24 -0400

    wrapper class

---
+++ NPF-4/wrapper.swift
@@ -0,0 +1,13 @@
+//
+//  wrapper.swift
+//  NPF-4
+//
+//  Created by Chetan R Kanthala on 5/4/17.
+//  Copyright © 2017 Chetan R Kanthala. All rights reserved.
+//
+
+import Foundation
+
+class Wrapper {
+    var parkList : [Park]?
+}
